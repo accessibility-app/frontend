534fc0a757f87204cf103673c3f556dee3bae0e5
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-08-10 11:36:32 +0100

    feature: places lib

---
+++ src/lib/data/places.tsx
@@ -0,0 +1,150 @@
+export const places = [
+  {
+    id: '351b8e69-58d5-4e8d-8072-0cb731b00582',
+    name: 'Accessibility toilet 5th floor, SIWB',
+    picture: 'src/assets/images/Accessibility toilet  5th floor, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13353,
+      longitude: 57.11886,
+    },
+  },
+  {
+    id: '6b1e8e99-5657-450d-aea7-53c8770bce75',
+    name: 'Accessibility toilet 4th floor (Gents), SIWB',
+    picture:
+      'src/assets/images/Accessibility toilet 4th floor (Gents), SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13467,
+      longitude: 57.11866,
+    },
+  },
+  {
+    id: '64966901-b527-4c68-8b5e-ef4ce3a4157c',
+    name: 'Break out Space 4th Floor, SIWB',
+    picture: 'src/assets/images/Break out space 4th Floor, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13361,
+      longitude: 57.11854,
+    },
+  },
+  {
+    id: 'f599ad71-bb4b-4ddf-b953-145d91ee04d6',
+    name: 'Engineering processing & membranes, SIWB',
+    picture: 'src/assets/images/Engineering processing & membranes, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13483,
+      longitude: 57.11872,
+    },
+  },
+  {
+    id: '41871bcf-629e-4dd5-8446-ec8a86145fa3',
+    name: 'Hallway to lobby 5th floor school of computing, SIWB',
+    picture:
+      'src/assets/images/Hallway to Lobby 5th floor school of computing, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13352,
+      longitude: 57.11881,
+    },
+  },
+  {
+    id: '58934047-4207-4619-af1b-d94c93672446',
+    name: 'Ladies toilet 5th floor, SIWB',
+    picture: 'src/assets/images/Ladies toilet 5th floor, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.1336,
+      longitude: 57.11857,
+    },
+  },
+  {
+    id: '05e9079e-6c1e-4c74-8948-13743baac503',
+    name: 'Lift access on 4th floor, SIWB',
+    picture: 'src/assets/images/Lift access on 4th floor, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13458,
+      longitude: 57.11873,
+    },
+  },
+  {
+    id: 'abdc5f32-abde-4175-8584-e9c370dea032',
+    name: 'Men toilet 4th floor, SIWB',
+    picture: 'src/assets/images/Men toilet 4th floor, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.013357,
+      longitude: 57.11857,
+    },
+  },
+  {
+    id: 'fb29f5b6-90a8-4a54-a012-b6e1fb3711e0',
+    name: 'N425 IT Support school of computing 4th Floor, SIWB',
+    picture:
+      'src/assets/images/N425 IT Support school of computing 4th Floor, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13352,
+      longitude: 57.11873,
+    },
+  },
+  {
+    id: 'c85704b4-0e90-4cd5-96d9-55add0e652f1',
+    name: 'N404 Engineering electric stores, SIWB',
+    picture: 'src/assets/images/N404 Engineering electric stores, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13483,
+      longitude: 57.11872,
+    },
+  },
+  {
+    id: 'ce7d0fc8-8e33-41de-8b9f-1e26956c74d8',
+    name: 'N529 PG Computing lab, SIWB',
+    picture: 'src/assets/images/N529 PG Computing lab, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13342,
+      longitude: 57.11877,
+    },
+  },
+  {
+    id: 'cda49be4-625a-4681-af62-a96ae6c34709',
+    name: 'N530 multimedia lab, SIWB',
+    picture: 'src/assets/images/N530 multimedia lab, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13339,
+      longitude: 57.11878,
+    },
+  },
+  {
+    id: 'c49da036-f10f-42f4-b9f0-42ad73620408',
+    name: 'N532 School of computing, SIWB',
+    picture: 'src/assets/images/N532 School of computing, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13346,
+      longitude: 57.11878,
+    },
+  },
+  {
+    id: '9036e2f3-621b-4b01-8e3c-b8a04a1f7a01',
+    name: 'N533, Foundation year lab, School of computing, SIWB',
+    picture:
+      'src/assets/images/N533, Foundation year lab, School of computing, SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13306,
+      longitude: 57.11899,
+    },
+  },
+  {
+    id: 'c1671b97-57ed-4c01-8ea2-486621deb95e',
+    name: 'SIWB, the sitting area outside',
+    picture: 'src/assets/images/SIWB, the Sitting Area outside.jpeg',
+    coordinates: {
+      latitude: 2.13521,
+      longitude: 57.11917,
+    },
+  },
+  {
+    id: '7c06aa92-aecd-4bf2-855d-412859c34b7a',
+    name: 'Walkway into SIWB',
+    picture: 'src/assets/images/Walkway into SIWB.jpeg',
+    coordinates: {
+      latitude: 2.13491,
+      longitude: 57.11862,
+    },
+  },
+];
