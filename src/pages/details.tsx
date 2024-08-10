992e755350380d2045f5f4c5f3629def82559611
wills <godswillijezei@gmail.com>
2024-08-10 12:11:54 +0100

    place details

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom"
+import { places } from "../lib/data/places"
 
 const PlaceDetails = () => {
     const {id} = useParams()
+    const place = places.find((where) => where.id === `${id}`)
   return (
     <div className="flex lg:h-screen w-full">
         <div className="w-5/12">
