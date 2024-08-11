7822a92fe18b16c5ff987743d6468f89903cac7e
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-08-11 21:15:53 +0100

    correction: place data

--- src/lib/data/places.tsx
+++ src/lib/data/places.tsx
@@ -1,163 +1,249 @@
-import toilet5 from "../../assets/images/Accessibility toilet  5th floor, SIWB.jpeg";
-import toilet4 from "../../assets/images/Accessibility toilet 4th floor (Gents), SIWB.jpeg";
-import space4 from "../../assets/images/Break out space 4th Floor, SIWB.jpeg";
-import membrane from "../../assets/images/Engineering processing & membranes, SIWB.jpeg";
-import computing5 from "../../assets/images/Hallway to Lobby 5th floor school of computing, SIWB.jpeg";
-import ladies5 from "../../assets/images/Ladies toilet 5th floor, SIWB.jpeg";
-import floor4 from "../../assets/images/Lift access on 4th floor, SIWB.jpeg";
-import mens4 from "../../assets/images/Men toilet 4th floor, SIWB.jpeg";
-import estore from "../../assets/images/N404 Engineering electric stores, SIWB.jpeg";
-import compute4 from "../../assets/images/N425 IT Support school of computing 4th Floor, SIWB.jpeg";
-import computelab from "../../assets/images/N529 PG Computing lab, SIWB.jpeg";
-import medialab from "../../assets/images/N530 multimedia lab, SIWB.jpeg";
-import n532compute from "../../assets/images/N532 School of computing, SIWB.jpeg";
-import n533lab from "../../assets/images/N533, Foundation year lab, School of computing, SIWB.jpeg";
-import area from "../../assets/images/SIWB, the Sitting Area outside.jpeg";
-import walkway from "../../assets/images/Walkway into SIWB.jpeg";
+import toilet5 from '../../assets/images/Accessibility toilet  5th floor, SIWB.jpeg';
+import toilet4 from '../../assets/images/Accessibility toilet 4th floor (Gents), SIWB.jpeg';
+import space4 from '../../assets/images/Break out space 4th Floor, SIWB.jpeg';
+import membrane from '../../assets/images/Engineering processing & membranes, SIWB.jpeg';
+import computing5 from '../../assets/images/Hallway to Lobby 5th floor school of computing, SIWB.jpeg';
+import ladies5 from '../../assets/images/Ladies toilet 5th floor, SIWB.jpeg';
+import floor4 from '../../assets/images/Lift access on 4th floor, SIWB.jpeg';
+import mens4 from '../../assets/images/Men toilet 4th floor, SIWB.jpeg';
+import estore from '../../assets/images/N404 Engineering electric stores, SIWB.jpeg';
+import compute4 from '../../assets/images/N425 IT Support school of computing 4th Floor, SIWB.jpeg';
+import computelab from '../../assets/images/N529 PG Computing lab, SIWB.jpeg';
+import medialab from '../../assets/images/N530 multimedia lab, SIWB.jpeg';
+import n532compute from '../../assets/images/N532 School of computing, SIWB.jpeg';
+import n533lab from '../../assets/images/N533, Foundation year lab, School of computing, SIWB.jpeg';
+import area from '../../assets/images/SIWB, the Sitting Area outside.jpeg';
+import walkway from '../../assets/images/Walkway into SIWB.jpeg';
+import liftfloor3 from '../../assets/images/Lift access 3rd floor and staircase to floor 2 A_B.jpeg';
+import reception from '../../assets/images/SIWB Reception.jpeg';
 
 export const places = [
   {
-    id: "351b8e69-58d5-4e8d-8072-0cb731b00582",
-    name: "Accessibility toilet 5th floor, SIWB",
+    id: '351b8e69-58d5-4e8d-8072-0cb731b00582',
+    name: 'Accessibility toilet 5th floor, SIWB',
     picture: toilet5,
+    description:
+      'Designed to meet the needs of people with disabilities. Door opens outwards with wider entry for a wheelchair user. Presence of grab bars for positioning and stablilty. Clear signage on entrance. Toilet paper placed within easy reach. Available emergency pull cord',
+    extra: '5th floor',
     coordinates: {
       latitude: -2.13353,
       longitude: 57.11886,
     },
   },
   {
-    id: "6b1e8e99-5657-450d-aea7-53c8770bce75",
-    name: "Accessibility toilet 4th floor (Gents), SIWB",
+    id: '351b8e69-58d5-4e8d-8072-0cb731b00582',
+    name: 'Accessibility toilet 5th floor, SIWB',
+    picture: toilet5,
+    description:
+      'Designed to meet the needs of people with disabilities. Door opens outwards with wider entry for a wheelchair user. Presence of grab bars for positioning and stablilty. Clear signage on entrance. Toilet paper placed within easy reach. Available emergency pull cord',
+    extra: '4th floor',
+    coordinates: {
+      latitude: -2.134667,
+      longitude: 57.118667,
+    },
+  },
+  {
+    id: '6b1e8e99-5657-450d-aea7-53c8770bce75',
+    name: 'Accessibility toilet 4th floor (Gents), SIWB',
+    description:
+      'Designed to meet the needs of people identified as male with disabilities. Door opens outwards with wider entry for a wheelchair. Presence of grab bars for positioning and stablilty. Urinary available with a toilet. Clear signage on entrance. Toilet paper placed within easy reach. Presence of emergency pull cord',
     picture: toilet4,
+    extra: '4th floor',
     coordinates: {
-      latitude: -2.13467,
-      longitude: 57.11866,
+      latitude: -2.134667,
+      longitude: 57.118667,
     },
   },
   {
-    id: "64966901-b527-4c68-8b5e-ef4ce3a4157c",
-    name: "Break out Space 4th Floor, SIWB",
+    id: '64966901-b527-4c68-8b5e-ef4ce3a4157c',
+    name: 'Break out Space 4th Floor, SIWB',
+    description:
+      'Multifunctional area on 4th floor used for informal meetings or casual sessions. It also serves as a relaxing area to stretch. It has a water refill corner. A print machine by the corner for quick prints, photocopying and scanning . it works by key in your username and uniflow printing PIN',
     picture: space4,
+    extra: '4th floor',
     coordinates: {
-      latitude: -2.13361,
-      longitude: 57.11854,
+      latitude: -2.133611,
+      longitude: 57.118528,
     },
   },
   {
-    id: "f599ad71-bb4b-4ddf-b953-145d91ee04d6",
-    name: "Engineering processing & membranes, SIWB",
+    id: 'f599ad71-bb4b-4ddf-b953-145d91ee04d6',
+    name: 'Engineering processing & membranes, SIWB',
+    description:
+      'It is an Engineering lab set up for different uses such as testing properties of membrane, simulating processes (biological and chemical). Some apparatus found in this room are membrane moodles, sensors and vessels.',
     picture: membrane,
+    extra: '4th floor',
     coordinates: {
-      latitude: -2.13483,
-      longitude: 57.11872,
+      latitude: -2.134833,
+      longitude: 57.118722,
     },
   },
   {
-    id: "41871bcf-629e-4dd5-8446-ec8a86145fa3",
-    name: "Hallway to lobby 5th floor school of computing, SIWB",
+    id: '41871bcf-629e-4dd5-8446-ec8a86145fa3',
+    name: 'Hallway to lobby 5th floor school of computing, SIWB',
+    description:
+      'This hallway measures 178cm x 501cm carpeted with a rug. It leads from the end of room N533 to the lobby acess where there is a lift that leads to lower levels of the floor. It is also leads to emergency fire escape route.',
     picture: computing5,
+    extra: '5th floor',
     coordinates: {
-      latitude: -2.13352,
-      longitude: 57.11881,
+      latitude: -2.133528,
+      longitude: 57.118806,
     },
   },
   {
-    id: "58934047-4207-4619-af1b-d94c93672446",
-    name: "Ladies toilet 5th floor, SIWB",
+    id: '58934047-4207-4619-af1b-d94c93672446',
+    name: 'Ladies toilet 5th floor, SIWB',
+    description:
+      'Has a fireproof door, space wide enough to contain atleast two body size at a time. Has a wide mirror, two toilets seperated by a privacy divider, sinks, sanitary bin and hand dryer. Poster/notice for information purpose, toilet paper and sanitary products are available',
     picture: ladies5,
+    extra: '5th floor',
     coordinates: {
-      latitude: -2.1336,
-      longitude: 57.11857,
+      latitude: -2.133611,
+      longitude: 57.118583,
     },
   },
   {
-    id: "05e9079e-6c1e-4c74-8948-13743baac503",
-    name: "Lift access on 4th floor, SIWB",
+    id: '05e9079e-6c1e-4c74-8948-13743baac503',
+    name: 'Lift access on 4th floor, SIWB',
+    description:
+      'Lift access on level floor which can be used to aceess level 1, 2, 3 or 5. Minimal noise, has auditory notification for announcement on each floor onto. Max Capacity of 1,275 kg or 17 Persons at a time. There is a hearing loop for user with a hearing impairment. When switched, it aids in hearing ',
     picture: floor4,
+    extra: '4th floor',
     coordinates: {
-      latitude: -2.13458,
-      longitude: 57.11873,
+      latitude: -2.134583,
+      longitude: 57.118722,
     },
   },
   {
-    id: "abdc5f32-abde-4175-8584-e9c370dea032",
-    name: "Men toilet 4th floor, SIWB",
+    id: 'abdc5f32-abde-4175-8584-e9c370dea032',
+    name: 'Men toilet 4th floor, SIWB',
+    description:
+      'Has a fireproof door, space wide enough to contain atleast two body size at a time. There are two toilets seprated by a privacy divider, no urinals. There are toilet papers. Has a wide mirror, sinks and hand dryer. Poster/notice for information purpose.',
     picture: mens4,
+    extra: '4th floor',
     coordinates: {
-      latitude: -2.013357,
-      longitude: 57.11857,
+      latitude: -2.133583,
+      longitude: 57.118583,
     },
   },
   {
-    id: "fb29f5b6-90a8-4a54-a012-b6e1fb3711e0",
-    name: "N425 IT Support school of computing 4th Floor, SIWB",
+    id: 'fb29f5b6-90a8-4a54-a012-b6e1fb3711e0',
+    name: 'N425 IT Support school of computing 4th Floor, SIWB',
+    description:
+      'Entrance to School of Computing IT support office for general enquiries and and related enquiries about system issues, support or troubleshootings. It is open to students and staff. ',
     picture: compute4,
+    extra: '4th floor',
     coordinates: {
-      latitude: -2.13352,
-      longitude: 57.11873,
+      latitude: -2.133528,
+      longitude: 57.118722,
     },
   },
   {
-    id: "c85704b4-0e90-4cd5-96d9-55add0e652f1",
-    name: "N404 Engineering electric stores, SIWB",
+    id: 'c85704b4-0e90-4cd5-96d9-55add0e652f1',
+    name: 'N404 Engineering electric stores, SIWB',
+    description:
+      'It collaborates with engineering lab for providing relevant materials to students or staff during lab sessions. Some materials in the store includes micrometer, computer, transistors and circuit boards. Acess is by a key or by an approved personnel to open when necessary.',
     picture: estore,
+    extra: '4th floor',
     coordinates: {
-      latitude: -2.13483,
-      longitude: 57.11872,
+      latitude: -2.134833,
+      longitude: 57.118722,
     },
   },
   {
-    id: "ce7d0fc8-8e33-41de-8b9f-1e26956c74d8",
-    name: "N529 PG Computing lab, SIWB",
+    id: 'ce7d0fc8-8e33-41de-8b9f-1e26956c74d8',
+    name: 'N529 PG Computing lab, SIWB',
+    description:
+      'It is a specialist sctivity room for PG students. Some other approved computing activities can be conducted here.  Currently closed. It has 12 units of computers on each 12 seater desk surrounded at both ends  of the room a 6 units on a 6 seater desk  ',
     picture: computelab,
+    extra: '5th floor',
     coordinates: {
-      latitude: -2.13342,
-      longitude: 57.11877,
+      latitude: -2.133417,
+      longitude: 57.118778,
     },
   },
   {
-    id: "cda49be4-625a-4681-af62-a96ae6c34709",
-    name: "N530 multimedia lab, SIWB",
+    id: 'cda49be4-625a-4681-af62-a96ae6c34709',
+    name: 'N530 multimedia lab, SIWB',
+    description:
+      'It is the schools Multimedia and Specialist IT lab. Priority is given to students who needs  a specialist software. There are 6 units of desktop computers on a 6 seater table joined together by a 6 seater table of 6 units of desktop computers. Currently closed for rebuilding.',
     picture: medialab,
+    extra: '5th floor',
     coordinates: {
-      latitude: -2.13339,
-      longitude: 57.11878,
+      latitude: -2.13347,
+      longitude: 57.118753,
     },
   },
   {
-    id: "c49da036-f10f-42f4-b9f0-42ad73620408",
-    name: "N532 School of computing, SIWB",
+    id: 'c49da036-f10f-42f4-b9f0-42ad73620408',
+    name: 'N532 School of computing, SIWB',
+    description:
+      'Door to School of Computing Audio Visual store. It is only accessible using a key by an authorised personnel. It is used to store inventories like projectors, adaptors, monitors, AV cables and other relevant materials',
     picture: n532compute,
+    extra: '5th floor',
     coordinates: {
-      latitude: -2.13346,
-      longitude: 57.11878,
+      latitude: -2.133472,
+      longitude: 57.118778,
     },
   },
   {
-    id: "9036e2f3-621b-4b01-8e3c-b8a04a1f7a01",
-    name: "N533, Foundation year lab, School of computing, SIWB",
+    id: '9036e2f3-621b-4b01-8e3c-b8a04a1f7a01',
+    name: 'N533, Foundation year lab, School of computing, SIWB',
+    description:
+      'This is a foundation year lab. Most seating arrangement is a 4 desktop computer units in 1 station.  In recent times, it has been used to conduct lectures and examinations involving large number of students. It has a public address system for teaching and announcements with big large screen displays across corners of the room ',
     picture: n533lab,
+    extra: '5th floor',
     coordinates: {
-      latitude: -2.13306,
-      longitude: 57.11899,
+      latitude: -2.133056,
+      longitude: 57.119,
     },
   },
   {
-    id: "c1671b97-57ed-4c01-8ea2-486621deb95e",
-    name: "SIWB, the sitting area outside",
+    id: 'c1671b97-57ed-4c01-8ea2-486621deb95e',
+    name: 'SIWB, the sitting area outside',
+    description:
+      'Outdoor sitting area for relaxation and interactions. The benches and tables surrounding it are made of wood. The environ has smooth flooring designed for all users and those with reduced mobility. It also leads to SIWB entrance',
     picture: area,
+    extra: 'Outside campus premises',
     coordinates: {
-      latitude: -2.13521,
-      longitude: 57.11917,
+      latitude: -2.135222,
+      longitude: 57.119167,
     },
   },
   {
-    id: "7c06aa92-aecd-4bf2-855d-412859c34b7a",
-    name: "Walkway into SIWB",
+    id: '7c06aa92-aecd-4bf2-855d-412859c34b7a',
+    name: 'Walkway into SIWB',
+    decription:
+      'This is a walkway after the reception. It has a Galicia vinyl floor finish with grey patches. There are display boards information on the sides. I also serves as an entrance to lift access, staircase, changing room, toilets and most lecture and staff rooms',
     picture: walkway,
+    extra: '3rd floor',
     coordinates: {
-      latitude: -2.13491,
-      longitude: 57.11862,
+      latitude: -2.134917,
+      longitude: 57.118611,
+    },
+  },
+  {
+    id: '009a21bc-3e04-42dc-b0b6-45326e8505e5',
+    name: 'Lift access 3rd floor and staircase to floor 2 A/B',
+    decription:
+      'Lift access from 3rd floor (Level 3) to floor 1, 2 ,4 or 5. There is also a staircase available leading to floor 2A. This is located on the right side of the building walking in from the entrance of SIWB (3rd fllor)',
+    picture: liftfloor3,
+    extra: '3rd floor',
+    coordinates: {
+      latitude: -2.13466,
+      longitude: 57.11886,
+    },
+  },
+  {
+    id: '009a21bc-3e04-42dc-b0b6-45326e8505e5',
+    name: 'SIWB Reception',
+    decription:
+      'SIWB reception. It serves as a help point for students, staff, visitors, deliveries or general guidance on about the faculty and the school at large. It is accessible via the three (3) Mnatrap door and an accessibility door from the entrance',
+    picture: reception,
+    extra: '3rd floor',
+    coordinates: {
+      latitude: -2.13518,
+      longitude: 57.11874,
     },
   },
 ];
